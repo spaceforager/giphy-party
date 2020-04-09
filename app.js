b826f03c64a8cdce4028d92016541b6047351c39
Sauvik Chakraborty <sc@email>
2020-04-09 02:57:32 -0400

    needs review

---
+++ app.js
@@ -0,0 +1,41 @@
+const gifDisplay = document.querySelector('#gif-disp');
+
+// make AJAX call
+async function search(term) {
+	const res = await axios.get(
+		`http://api.giphy.com/v1/gifs/search?q=${term}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+	);
+	return res.data;
+}
+
+// Append gif result to a created div
+function addGIF(result) {
+	let dataLength = result.data.length;
+
+	if (dataLength) {
+		let randIndex = Math.floor(Math.random() * dataLength);
+		let newCol = document.createElement('DIV');
+		newCol.classList.add('col-md-4', 'col-12', 'mb-4');
+		let newGIF = document.createElement('IMG');
+		newGIF.setAttribute('src', `${result.data[randIndex].url}`);
+
+		newCol.appendChild(newGIF);
+		gifDisplay.appendChild(newCol);
+	}
+}
+
+// handle form submission and clear search box
+const form = document.querySelector('#form');
+form.addEventListener('submit', async function(e) {
+	e.preventDefault();
+	let input = document.querySelector('#inp');
+
+	const newAPIData = await search(input.value);
+	addGIF(newAPIData);
+	input.value = '';
+});
+
+const $gifDisplay = $(gifDisplay);
+$('#rem').on('click', function() {
+	$gifDisplay.empty();
+});
